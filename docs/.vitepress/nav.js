7833b3c21af50e2e23def442d653496bf817199c
yangwan-cw <a2572213252@gmail.com>
2024-12-09 11:04:38 +0800

    feat: 优化冗余字段，增加语言学习前言

--- docs/.vitepress/nav.js
+++ docs/.vitepress/nav.js
@@ -3,118 +3,147 @@ import { SiPwa } from 'react-icons/si';
 export default [
   {
     text: '<i class="fa-solid fa-code" style="margin-right: 10px; color: #4A90E2;"></i>前端开发', items: [
-      {
-        text: '基础知识',items: [
-          { text: '<i class="fab fa-html5" style="margin-right: 10px; color: #E44D26;"></i>Html', link: '/前端开发/Html/基础/1.引言' },
-          { text: '<i class="fab fa-html5" style="margin-right: 10px; color: #E44D26;"></i>Html5', link: '/前端开发/Html/Html5/1.Html5' },
-          { text: '<i class="fab fa-css3-alt" style="margin-right: 10px; color: #264DE4;"></i>Css', link: '/前端开发/Css/index', },
-          { text: '<i class="fab fa-css3-alt" style="margin-right: 10px; color: #264DE4;"></i>Css3', link: '/前端开发/Css3/1.Css3' },
-          { text: '<i class="fab fa-js" style="margin-right: 10px; color: #F7DF1E;"></i>JavaScript', link: '/markdown-examples' },
-        ]
-      },
-      {
-        text: '框架', items: [
-          { text: '<i class="fab fa-js" style="margin-right: 10px; color: #3178C6;"></i>TypeScript', link: '/markdown-examples' },
-          { text: '<i class="fab fa-react" style="margin-right: 10px; color: #61DAFB;"></i>React', link: '/api-examples' },
-          { text: '<i class="fab fa-vuejs" style="margin-right: 10px; color: #4FC08D;"></i>Vue 2 & 3', link: '/api-examples' },
-        ]
-      },
-      {
-        text: '服务端', items: [
-          { text: '<i class="fab fa-node-js" style="margin-right: 10px; color: #339933;"></i>Node', link: '/markdown-examples' },
-          { text: '<i class="fab fa-node-js" style="margin-right: 10px; color: #339933;"></i>Express', link: '/markdown-examples' },
-        ]
-      },
-      {
-        text: '小程序 & app & 桌面端', items: [
-          { text: '<i class="fab fa-weixin" style="margin-right: 10px; color: #07C160;"></i>公众号 & 小程序开发', link: '/api-examples' },
-          { text: '<i class="fab fa-flutter" style="margin-right: 10px; color: #02569B;"></i>Flutter', link: '/api-examples' },
-          { text: '<i class="fa-solid fa-desktop" style="margin-right: 10px; color: #47848F;"></i>Electron', link: '/markdown-examples' },
-        ]
-      },
-      {
-        text: '工具 & 类库', items: [
-          { text: '<i class="fa-solid fa-cube" style="margin-right: 10px; color: #8DD6F9;"></i>Webpack', link: '/markdown-examples' },
-          { text: '<i class="fa-solid fa-bolt" style="margin-right: 10px; color: #646CFF;"></i>Vite', link: '/markdown-examples' },
-          { text: '<i class="fab fa-vuejs" style="margin-right: 10px; color: #3EAF7C;"></i>VuePress', link: '/markdown-examples' },
-          { text: '<i class="fab fa-vuejs" style="margin-right: 10px; color: #3EAF7C;"></i>VitePress', link: '/markdown-examples' },
-        ]
-      },
-      {
-        text: '其他', items: [
-          { text: '<i class="fab fa-docker" style="margin-right: 10px; color: #2496ED;"></i>Docker', link: '/markdown-examples' },
-          { text: '<i class="fa-solid fa-server" style="margin-right: 10px; color: #009639;"></i>Nginx', link: '/markdown-examples' },
-          { text: '<i class="fa-solid fa-gauge-high" style="margin-right: 10px; color: #FF4081;"></i>性能优化', link: '/markdown-examples' },
-          { text: '<i class="fa-solid fa-puzzle-piece" style="margin-right: 10px; color: #1976D2;"></i>微前端', link: '/markdown-examples' },
-          { text: '<i class="fa-solid fa-shield-alt" style="margin-right: 10px; color: #4CAF50;"></i>前端安全', link: '/markdown-examples' },
-          { text: '<i class="fa-solid fa-mobile-alt" style="margin-right: 10px; color: #7B1FA2;"></i>PWA', link: '/markdown-examples' },
-          { text: '<i class="fa-solid fa-plug" style="margin-right: 10px; color: #FF9800;"></i>插件', link: '/markdown-examples' },
-        ]
-      }
+      { text: '', items:[
+        { text: '<i class="fa-solid fa-comments" style="margin-right: 10px; color: #009A61;"></i>学习知识前言', link: '/1.前端开发/前言/index' },
+      ] },
+      { text: '学习路线', items:[
+        { text: '<i class="fa-solid fa-map-location-dot" style="margin-right: 10px; color: #F9AB00;"></i>前端学习路线', link: '/前端开发/学习路线/1.前端学习路线' },
+      ] },
+      // {
+      //   text: '基础知识',items: [
+      //     { text: '<i class="fab fa-html5" style="margin-right: 10px; color: #E44D26;"></i>Html', link: '/前端开发/Html/基础/1.引言' },
+      //     // { text: '<i class="fab fa-html5" style="margin-right: 10px; color: #E44D26;"></i>Html5', link: '/前端开发/Html/Html5/1.Html5' },
+      //     // { text: '<i class="fab fa-css3-alt" style="margin-right: 10px; color: #264DE4;"></i>Css', link: '/前端开发/Css/index', },
+      //     // { text: '<i class="fab fa-css3-alt" style="margin-right: 10px; color: #264DE4;"></i>Css3', link: '/前端开发/Css3/1.Css3' },
+      //     // { text: '<i class="fab fa-js" style="margin-right: 10px; color: #F7DF1E;"></i>JavaScript', link: '/markdown-examples' },
+      //   ]
+      // },
+      // {
+      //   text: '框架', items: [
+      //     { text: '<i class="fab fa-js" style="margin-right: 10px; color: #3178C6;"></i>TypeScript', link: '/markdown-examples' },
+      //     { text: '<i class="fab fa-react" style="margin-right: 10px; color: #61DAFB;"></i>React', link: '/api-examples' },
+      //     { text: '<i class="fab fa-vuejs" style="margin-right: 10px; color: #4FC08D;"></i>Vue 2 & 3', link: '/api-examples' },
+      //   ]
+      // },
+      // {
+      //   text: '服务端', items: [
+      //     { text: '<i class="fab fa-node-js" style="margin-right: 10px; color: #339933;"></i>Node', link: '/markdown-examples' },
+      //     { text: '<i class="fab fa-node-js" style="margin-right: 10px; color: #339933;"></i>Express', link: '/markdown-examples' },
+      //   ]
+      // },
+      // {
+      //   text: '小程序 & app & 桌面端', items: [
+      //     { text: '<i class="fab fa-weixin" style="margin-right: 10px; color: #07C160;"></i>公众号 & 小程序开发', link: '/api-examples' },
+      //     { text: '<i class="fab fa-flutter" style="margin-right: 10px; color: #02569B;"></i>Flutter', link: '/api-examples' },
+      //     { text: '<i class="fa-solid fa-desktop" style="margin-right: 10px; color: #47848F;"></i>Electron', link: '/markdown-examples' },
+      //   ]
+      // },
+      // {
+      //   text: '工具 & 类库', items: [
+      //     { text: '<i class="fa-solid fa-cube" style="margin-right: 10px; color: #8DD6F9;"></i>Webpack', link: '/markdown-examples' },
+      //     { text: '<i class="fa-solid fa-bolt" style="margin-right: 10px; color: #646CFF;"></i>Vite', link: '/markdown-examples' },
+      //     { text: '<i class="fab fa-vuejs" style="margin-right: 10px; color: #3EAF7C;"></i>VuePress', link: '/markdown-examples' },
+      //     { text: '<i class="fab fa-vuejs" style="margin-right: 10px; color: #3EAF7C;"></i>VitePress', link: '/markdown-examples' },
+      //   ]
+      // },
+      // {
+      //   text: '其他', items: [
+      //     { text: '<i class="fab fa-docker" style="margin-right: 10px; color: #2496ED;"></i>Docker', link: '/markdown-examples' },
+      //     { text: '<i class="fa-solid fa-server" style="margin-right: 10px; color: #009639;"></i>Nginx', link: '/markdown-examples' },
+      //     { text: '<i class="fa-solid fa-gauge-high" style="margin-right: 10px; color: #FF4081;"></i>性能优化', link: '/markdown-examples' },
+      //     { text: '<i class="fa-solid fa-puzzle-piece" style="margin-right: 10px; color: #1976D2;"></i>微前端', link: '/markdown-examples' },
+      //     { text: '<i class="fa-solid fa-shield-alt" style="margin-right: 10px; color: #4CAF50;"></i>前端安全', link: '/markdown-examples' },
+      //     { text: '<i class="fa-solid fa-mobile-alt" style="margin-right: 10px; color: #7B1FA2;"></i>PWA', link: '/markdown-examples' },
+      //     { text: '<i class="fa-solid fa-plug" style="margin-right: 10px; color: #FF9800;"></i>插件', link: '/markdown-examples' },
+      //   ]
+      // }
     ]
   },
   {
     text: '<i class="fa-solid fa-server" style="margin-right: 10px; color: #000000;"></i>后端开发', items: [
-      {
-        text: 'Java 基础知识', items: [
-          { text: '<i class="fab fa-java" style="margin-right: 10px; color: #007396;"></i>Java', link: '/后端知识体系/Java/index' },
-          { text: '<i class="fa-solid fa-microchip" style="margin-right: 10px; color: #ED8B00;"></i>JVM', link: '/后端知识体系/Jvm/index' },
-          { text: '<i class="fa-solid fa-network-wired" style="margin-right: 10px; color: #FF5722;"></i>并发编程', link: '/后端知识体系/并发编程/index' },
-          { text: '<i class="fa-solid fa-shapes" style="margin-right: 10p x; color: #4CAF50;"></i>设计模式', link: '/后端知识体系/设计模式/index' },
-        ]
-      },
+      { text: '', items:[
+        { text: '<i class="fa-solid fa-comments" style="margin-right: 10px; color: #009A61;"></i>学习知识前言', link: '/2.后端开发/前言/index' },
+      ] },
+      // { text: '学习路线', items:[
+      //   { text: '<i class="fa-solid fa-map-location-dot" style="margin-right: 10px; color: #F9AB00;"></i>后端学习路线', link: '/后端开发/学习路线/1.后端学习路线' },
+      // ] },
+      // {
+      //   text: 'Java 基础知识', items: [
+      //     { text: '<i class="fab fa-java" style="margin-right: 10px; color: #007396;"></i>Java', link: '/后端知识体系/Java/index' },
+      //     { text: '<i class="fa-solid fa-microchip" style="margin-right: 10px; color: #ED8B00;"></i>JVM', link: '/后端知识体系/Jvm/index' },
+      //     { text: '<i class="fa-solid fa-network-wired" style="margin-right: 10px; color: #FF5722;"></i>并发编程', link: '/后端知识体系/并发编程/index' },
+      //     { text: '<i class="fa-solid fa-shapes" style="margin-right: 10p x; color: #4CAF50;"></i>设计模式', link: '/后端知识体系/设计模式/index' },
+      //   ]
+      // },
   
-      {
-        text: '数据库', items: [
-          { text: '<i class="fa-solid fa-database" style="margin-right: 10px; color: #00758F;"></i>Mysql', link: '/后端知识体系/Mysql/index' },
-          { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #4DB33D;"></i>MongoDB', link: '/后端知识体系/MongoDB/index' },
-          { text: '<i class="fa-solid fa-bolt" style="margin-right: 10px; color: #DC382D;"></i>Redis', link: '/后端知识体系/Redis/index' },
-          { text: '<i class="fa-solid fa-magnifying-glass" style="margin-right: 10px; color: #FED10A;"></i>Elasticsearch', link: '/后端知识体系/ElasticSearch/index' },
-        ]
-      },
-      {
-        text: '框架', items: [
-          { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #6DB33F;"></i>Spring', link: '/后端知识体系/Spring/index' },
-          { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #6DB33F;"></i>SpringMVC', link: '/后端知识体系/SpringMVC/index' },
-          { text: '<i class="fa-solid fa-database" style="margin-right: 10px; color: #000000;"></i>Mybatis', link: '/后端知识体系/Mybatis/index' },
-          { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #6DB33F;"></i>SpringBoot', link: '/后端知识体系/SpringBoot/index' },
-          { text: '<i class="fa-solid fa-cloud" style="margin-right: 10px; color: #6DB33F;"></i>SpringCloud', link: '/后端知识体系/SpringCloud/index' },
-        ]
-      },
-      {
-        text: '中间件', items: [
-          { text: '<i class="fa-solid fa-paper-plane" style="margin-right: 10px; color: #FF6600;"></i>RabbitMQ', link: '/后端知识体系/RabbitMQ/index' },
-          { text: '<i class="fa-solid fa-message" style="margin-right: 10px; color: #231F20;"></i>Kafka', link: '/后端知识体系/Kafka/index' },
-        ]
-      },
-      {
-        text: '部署', items: [
-          { text: '<i class="fab fa-linux" style="margin-right: 10px; color: #FCC624;"></i>Linux', link: '/后端知识体系/linux/index' },
-          { text: '<i class="fab fa-docker" style="margin-right: 10px; color: #2496ED;"></i>Docker', link: '/后端知识体系/Docker/index' },
-          { text: '<i class="fa-solid fa-infinity" style="margin-right: 10px; color: #D24939;"></i>Jenkins', link: '/后端知识体系/Jenkins/index' },
-          { text: '<i class="fa-solid fa-server" style="margin-right: 10px; color: #009639;"></i>Nginx', link: '/后端知识体系/Nginx/index' },
-          { text: '<i class="fa-solid fa-cat" style="margin-right: 10px; color: #F8DC75;"></i>Tomcat', link: '/后端知识体系/Tomcat/index' },
-        ]
-      }
+      // {
+      //   text: '数据库', items: [
+      //     { text: '<i class="fa-solid fa-database" style="margin-right: 10px; color: #00758F;"></i>Mysql', link: '/后端知识体系/Mysql/index' },
+      //     { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #4DB33D;"></i>MongoDB', link: '/后端知识体系/MongoDB/index' },
+      //     { text: '<i class="fa-solid fa-bolt" style="margin-right: 10px; color: #DC382D;"></i>Redis', link: '/后端知识体系/Redis/index' },
+      //     { text: '<i class="fa-solid fa-magnifying-glass" style="margin-right: 10px; color: #FED10A;"></i>Elasticsearch', link: '/后端知识体系/ElasticSearch/index' },
+      //   ]
+      // },
+      // {
+      //   text: '框架', items: [
+      //     { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #6DB33F;"></i>Spring', link: '/后端知识体系/Spring/index' },
+      //     { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #6DB33F;"></i>SpringMVC', link: '/后端知识体系/SpringMVC/index' },
+      //     { text: '<i class="fa-solid fa-database" style="margin-right: 10px; color: #000000;"></i>Mybatis', link: '/后端知识体系/Mybatis/index' },
+      //     { text: '<i class="fa-solid fa-leaf" style="margin-right: 10px; color: #6DB33F;"></i>SpringBoot', link: '/后端知识体系/SpringBoot/index' },
+      //     { text: '<i class="fa-solid fa-cloud" style="margin-right: 10px; color: #6DB33F;"></i>SpringCloud', link: '/后端知识体系/SpringCloud/index' },
+      //   ]
+      // },
+      // {
+      //   text: '中间件', items: [
+      //     { text: '<i class="fa-solid fa-paper-plane" style="margin-right: 10px; color: #FF6600;"></i>RabbitMQ', link: '/后端知识体系/RabbitMQ/index' },
+      //     { text: '<i class="fa-solid fa-message" style="margin-right: 10px; color: #231F20;"></i>Kafka', link: '/后端知识体系/Kafka/index' },
+      //   ]
+      // },
+      // {
+      //   text: '部署', items: [
+      //     { text: '<i class="fab fa-linux" style="margin-right: 10px; color: #FCC624;"></i>Linux', link: '/后端知识体系/linux/index' },
+      //     { text: '<i class="fab fa-docker" style="margin-right: 10px; color: #2496ED;"></i>Docker', link: '/后端知识体系/Docker/index' },
+      //     { text: '<i class="fa-solid fa-infinity" style="margin-right: 10px; color: #D24939;"></i>Jenkins', link: '/后端知识体系/Jenkins/index' },
+      //     { text: '<i class="fa-solid fa-server" style="margin-right: 10px; color: #009639;"></i>Nginx', link: '/后端知识体系/Nginx/index' },
+      //     { text: '<i class="fa-solid fa-cat" style="margin-right: 10px; color: #F8DC75;"></i>Tomcat', link: '/后端知识体系/Tomcat/index' },
+      //   ]
+      // }
     ]
   },
   {
     text: '<i class="fa-solid fa-cubes" style="margin-right: 10px; color: #00599C;"></i>C++ 编程', items: [
-      { text: '<i class="fa-solid fa-code" style="margin-right: 10px; color: #00599C;"></i>C++', link: '/后端知识体系/C++/index' },
+      { text: '', items:[
+        { text: '<i class="fa-solid fa-comments" style="margin-right: 10px; color: #009A61;"></i>学习知识前言', link: '/3.C++编程/前言/index' },
+      ] },
+      // { text: '学习路线', items:[
+      //   { text: '<i class="fa-solid fa-map-location-dot" style="margin-right: 10px; color: #F9AB00;"></i>前端学习路线', link: '/前端开发/学习路线/1.前端学习路线' },
+      // ] },
+      // { text: '<i class="fa-solid fa-code" style="margin-right: 10px; color: #00599C;"></i>C++', link: '/后端知识体系/C++/index' },
     ]
   },
   {
     text: '<i class="fa-brands fa-python" style="margin-right: 10px; color: #FFD43B;"></i>Python 编程', items: [
-      { text: '<i class="fab fa-python" style="margin-right: 10px; color: #3776AB;"></i>Python', link: '/4.Python编程/index' },
+      { text: '', items:[
+        { text: '<i class="fa-solid fa-comments" style="margin-right: 10px; color: #009A61;"></i>学习知识前言', link: '/4.Python编程/前言/index' }
+      ] },
+      { text: '学习路线', items:[
+        { text: '<i class="fa-solid fa-map-location-dot" style="margin-right: 10px; color: #F9AB00;"></i>前端学习路线', link: '/前端开发/学习路线/1.前端学习路线' },
+      ] }
     ]
   },
   {
     text: '<i class="fa-solid fa-lightbulb" style="margin-right: 10px; color: #2196F3;"></i>零碎知识', items: [
       { text: '<i class="fa-solid fa-file-upload" style="margin-right: 10px; color: #2196F3;"></i>大文件上传', link: '/8.零碎知识/大文件上传&下载/index' },
+      { text: '<i class="fa-solid fa-file-upload" style="margin-right: 10px; color: #2196F3;"></i>websocket', link: '/8.零碎知识/websocket/index' },
+      { text: '<i class="fa-solid fa-code-compare" style="margin-right: 10px; color: black;"></i>Github Action', link: '/8.零碎知识/websocket/index' },
+      {text: '<i class="fa-solid fa-toolbox" style="margin-right: 10px; color: #FF5722;"></i>常用工具', link: '/8.零碎知识/常用工具/index'}
     ]
   },
   {
     text: '<i class="fa-solid fa-book" style="margin-right: 10px; color: #00599C;"></i>基础知识', items: [
+      { text: '', items:[
+        { text: '<i class="fa-solid fa-comments" style="margin-right: 10px; color: #009A61;"></i>学习知识前言', link: '/5.基础知识/前言/index' },
+      ] },
       {
         text: '基本概念', items: [
           { text: '编程概念', link: '/5.基础知识/编程概念/index' },
@@ -142,6 +171,9 @@ export default [
   },
   {
     text: '<i class="fa-solid fa-lightbulb" style="margin-right: 10px; color: #ffb800;"></i>知识拓展', items: [
+      { text: '', items:[
+        { text: '<i class="fa-solid fa-comments" style="margin-right: 10px; color: #009A61;"></i>学习知识前言', link: '/6.知识拓展/前言/index' },
+      ] },
       { text: '<i class="fa-solid fa-comments" style="margin-right: 10px; color: #2196F3;"></i>如何面试', link: '/零碎知识/如何面试' },
       { text: '<i class="fa-solid fa-file-alt" style="margin-right: 10px; color: #9C27B0;"></i>如何写简历', link: '/零碎知识/如何写简历' },
       { text: '<i class="fa-solid fa-code" style="margin-right: 10px; color: #FF5722;"></i>代码整洁之道', link: '/零碎知识/代码整洁之道' },
@@ -153,11 +185,6 @@ export default [
       { text: '<i class="fa-solid fa-book-open" style="margin-right: 10px; color: #FF6B6B;"></i>日记本', link: '/markdown-examples' },
       { text: '<i class="fa-solid fa-images" style="margin-right: 10px; color: #4ECDC4;"></i>相册', link: '/api-examples' },
       { text: '<i class="fa-solid fa-music" style="margin-right: 10px; color: #FF6B81;"></i>音乐', link: '/api-examples' }
-    ]
-  },
-  {
-    text: '<i class="fa-solid fa-folder" style="margin-right: 10px; color: #fd7e14;"></i>项目展示', items: [
-      { text: 'TodoList', link: '/markdown-examples' },
     ]
   },
   {
--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,41 +1,74 @@
-import { text } from "framer-motion/client";
-
 export default {
-    '/后端知识体系/Java/':[
+    '/1.前端开发/前言/':[
       {text:'',items:[
-        {text:'引言',link:'/后端知识体系/Java/基础/1.引言'},
-      ]}
-    ],
-    '/前端知识体系/Html/':[
-      {text:'学习路线',link:'/1.前端开发/1.Html/学习路线/index'},
-      {text:'HTML',items:[
-        {text:'引言',link:'/前端知识体系/Html/基础/1.引言'},
-        {text:'Html5',link:'/前端知识体系/Html/Html5/1.Html5'},
-      ]
-      },
-      {text:'HTML5',items:[
-        {text:'引言',link:'/前端知识体系/Html/Html5/1.Html5'},
-      ]}
-    ],
-    '/前端知识体系/Css/':[
-      {text:'CSS',items:[
-        {text:'引言',link:'/前端知识体系/Css3/基础/1.引言'},
+        {text:'前言',link:'/1.前端开发/前言/index'},
       ]},
-      {text:'CSS3',items:[
-        {text:'引言',link:'/前端知识体系/Css3/1.Css3'},
-      ]}
+    ],
+    '/2.后端开发/前言/':[
+      {text:'',items:[
+        {text:'前言',link:'/2.后端开发/前言/index'},
+      ]},
+    ],
+    '/3.C++编程/':[
+      {text:'',items:[
+        {text:'前言',link:'/3.C++编程/前言/index'},
+      ]},
     ],
     '/4.Python编程/':[
       {text:'',items:[
-        {text:'1. Python 学习路线',link:'/4.Python编程/Python学习路线/index'},
-        {text:'2. 引言',link:'/4.Python编程/index'},
-      ]}
+        {text:'前言',link:'/4.Python编程/前言/index'},
+      ]},
     ],
-    '/8.零碎知识/':[
-      {text:'前言',link:'/8.零碎知识/index'},
-      {text:'文件',items:[
-        {text:'文件上传 & 下载',link:'/8.零碎知识/大文件上传&下载/index'},
-        {text:'excel',link:'/8.零碎知识/excel/index'},
+    '/5.基础知识/前言/':[
+      {text:'',items:[
+        {text:'前言',link:'/5.基础知识/前言/index'},
+      ]},
+    ],
+    '/6.知识拓展/':[
+      {text:'',items:[
+        {text:'前言',link:'/6.知识拓展/前言/index'},
+      ]},
+    ],
+    '/8.零碎知识/常用工具':[
+      {text:'概述',link:'/8.零碎知识/常用工具/index'},
+      {text:'docker',items:[
+        {text:'docker',link:'/8.零碎知识/常用工具/docker/index'},
+        {text:'docker 因权限引发的线上问题',link:'/8.零碎知识/常用工具/docker/权限/index'},
+      ]},
+      {text:'git',items:[
+        {text:'git rebase & merge 的区别',link:'/8.零碎知识/常用工具/git/index'},
+        {text:'git 推送失败',link:'/8.零碎知识/常用工具/git/推送失败/index'},
+  
+      ]},
+      {text:'nginx',items:[
+        {text:'nginx',link:'/8.零碎知识/常用工具/nginx/index'},
+      ]},
+      {text:'vscode',items:[
+        {text:'vscode',link:'/8.零碎知识/常用工具/vscode/index'},
+        {text:'vscode 插件',link:'/8.零碎知识/常用工具/vscode/插件/index'},
+        {text:'vscode 快捷键',link:'/8.零碎知识/常用工具/vscode/快捷键/index'},
+        {text:'vscode 主题',link:'/8.零碎知识/常用工具/vscode/配置/index'},
+      ]},
+      {text:'shell',items:[
+        {text:'shell',link:'/8.零碎知识/常用工具/shell/index'},
+        {text:'shell 常用命令',link:'/8.零碎知识/常用工具/shell/常用命令/index'},
+        {text:'shell 打印远程服务器脚本',link:'/8.零碎知识/常用工具/shell/打印远程服务器脚本/index'},
+      ]},
+      {text:'镜像源',items:[
+        {text:'node 镜像源',link:'/8.零碎知识/常用工具/镜像源/node/index'},
+        {text:'npm 镜像源',link:'/8.零碎知识/常用工具/镜像源/npm/index'},
+        {text:'yarn 镜像源',link:'/8.零碎知识/常用工具/镜像源/yarn/index'},
+        {text:'pnpm 镜像源',link:'/8.零碎知识/常用工具/镜像源/pnpm/index'},
+        {text:'pip 镜像源',link:'/8.零碎知识/常用工具/镜像源/pip/index'},
+        {text:'conda 镜像源',link:'/8.零碎知识/常用工具/镜像源/conda/index'},
+        {text:'docker 镜像源',link:'/8.零碎知识/常用工具/镜像源/docker/index'},
+      ]},
+      {text:'idea & navicat等工具破解',items:[
+        {text:'idea破解',link:'/8.零碎知识/常用工具/idea破解/index'},
+        {text:'navicat破解',link:'/8.零碎知识/常用工具/navicat破解/index'},
+      ]},
+      {text: 'idea 插件 ',items:[
+        {text:'idea 插件',link:'/8.零碎知识/常用工具/idea插件/index'},
       ]},
     ],
     '/10.关于我/':[
