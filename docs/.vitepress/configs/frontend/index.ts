610c231259dc36edd42a0cfe2bac7225d6dbbe61
yanwan <a2572213252@gmail.com>
2025-06-08 23:52:37 +0800

    feat: 增加window 教程,增加 git 教程

--- docs/.vitepress/configs/frontend/index.ts
+++ docs/.vitepress/configs/frontend/index.ts
@@ -1,138 +1,122 @@
- const frontendConfig =  {
-        text: "前端",
-        items: [
-            {
-              text: "概述",
-              items:[
-                {
-                    text: "前端概述",
-                    link: '/frontend/',
-                },
-                {
-                  text:"前端基础知识",
-                    link: '/frontend/fundamentals',  
-                },
-                {
-                    text: "如何学习",
-                    link: '/frontend/how-to-learn',
-                },
-                {
-                    text: "官方文档",
-                    link: '/frontend/documentation',
-                }
-              ]  
-            },
-            {
-                text: "HTML",
-                items: [
-                    {
-                        text: "开始",
-                        link: '/frontend/html',
-                    },
-                    {
-                        text: "语义化标签",
-                        link: '/frontend/html',
-                    },
-                    {
-                        text: "新特性",
-                        link: '/frontend/',
-                    }
-                ]
-            },
-            {
-                text: "CSS",
-                items: [
-                    {
-                        text: "开始",
-                        link: '/frontend/css',
-                    },
-                    {
-                        text: "布局",
-                        link: '/frontend/css',
-                    },
-                    {
-                        text: "样式",
-                        link: '/frontend/css',
-                    },
-                    {
-                        text: "动画",
-                        link: '/frontend/css',
-                    },
-                    {
-                        text: "CSS3",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "预处理器",
-                        link: '/frontend/',
-                    },
-                ]
-            },
-            {
-                text: "JavaScript",
-                items: [
-                    {
-                        text: "基础",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "框架",
-                        link: '/frontend/',
-                    }
-                ]
-            },
-            {
-                text: "TypeScript",
-                items: [
-                    {
-                        text: "基础",
-                        link: '/frontend/',
-                    }
-                ]
-            },
-            {
-                text: "框架类库",
-                items:[
-                    {
-                        text: "Vue2",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "Vue3",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "React",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "微信小程序",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "Flutter",
-                        link: '/frontend/',
-                    },
-                ]
-            },
-            {
-                text: "工程",
-                items:[
-                    {
-                        text: "Webpack",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "Vite",
-                        link: '/frontend/',
-                    },
-                    {
-                        text: "Nginx",
-                        link: '/frontend/'
-                    }
-                ]
-            }
-        ]
-    }
+const frontendConfig = {
+  text: "前端",
+  items: [
+    {
+      text: "概述",
+      items: [
+        {
+          text: "前端概述",
+          link: "/frontend/",
+        },
+        {
+          text: "前端基础知识",
+          link: "/frontend/fundamentals",
+        },
+        {
+          text: "如何学习",
+          link: "/frontend/how-to-learn",
+        },
+        {
+          text: "官方文档",
+          link: "/frontend/documentation",
+        },
+      ],
+    },
+    {
+      text: "HTML",
+      items: [
+        {
+          text: "开始",
+          link: "/frontend/html",
+        },
+        {
+          text: "语义化标签",
+          link: "/frontend/html",
+        },
+        {
+          text: "新特性",
+          link: "/frontend/",
+        },
+      ],
+    },
+    {
+      text: "CSS",
+      items: [
+        {
+          text: "开始",
+          link: "/frontend/css",
+        },
+        {
+          text: "css3",
+          link: "/frontend/css3",
+        },
+      ],
+    },
+    {
+      text: "JavaScript",
+      items: [
+        {
+          text: "基础",
+          link: "/frontend/",
+        },
+        {
+          text: "框架",
+          link: "/frontend/",
+        },
+      ],
+    },
+    {
+      text: "TypeScript",
+      items: [
+        {
+          text: "基础",
+          link: "/frontend/",
+        },
+      ],
+    },
+    {
+      text: "框架类库",
+      items: [
+        {
+          text: "Vue2",
+          link: "/frontend/",
+        },
+        {
+          text: "Vue3",
+          link: "/frontend/",
+        },
+        {
+          text: "React",
+          link: "/frontend/",
+        },
+        {
+          text: "微信小程序",
+          link: "/frontend/",
+        },
+        {
+          text: "Flutter",
+          link: "/frontend/",
+        },
+      ],
+    },
+    {
+      text: "工程",
+      items: [
+        {
+          text: "Webpack",
+          link: "/frontend/",
+        },
+        {
+          text: "Vite",
+          link: "/frontend/",
+        },
+        {
+          text: "Nginx",
+          link: "/frontend/",
+        },
+      ],
+    },
+  ],
+};
 
-export default frontendConfig
+export default frontendConfig;
--- docs/.vitepress/configs/tools/index.ts
+++ docs/.vitepress/configs/tools/index.ts
@@ -1,21 +1,23 @@
-import type { DefaultTheme } from 'vitepress'
+import type { DefaultTheme } from "vitepress";
 
 const toolConfig: DefaultTheme.NavItemWithChildren = {
-    text: '工具',
-    items: [
-        { text: 'rabbitmq', link: '/tools/rabbitmq/' },
-        { text: 'vim', link: '/tools/vim/' },
-        { text: 'git', link: '/tools/git/' },
-        { text: 'maven', link: '/tools/maven/' },
-        { text: 'idea', link: '/tools/idea/' },
-        { text: 'Apifox', link: '/tools/apifox/' },
-        { text: 'Fiddler', link: '/tools/fiddler/' },
-        { text: 'Docker', link: '/tools/docker/' },
-        { text: 'Kubernetes', link: '/tools/kubernetes/' },
-        { text: 'Jenkins', link: '/tools/jenkins/' },
-        { text: 'Nginx', link: '/tools/nginx/' },
-        { text: 'Elasticsearch', link: '/tools/elasticsearch/' }
-    ]
-}
+  text: "工具",
+  items: [
+    { text: "rabbitmq", link: "/tools/rabbitmq/" },
+    { text: "vim", link: "/tools/vim/" },
+    { text: "git", link: "/tools/git/" },
+    { text: "maven", link: "/tools/maven/" },
+    { text: "idea", link: "/tools/idea/" },
+    { text: "Apifox", link: "/tools/apifox/" },
+    { text: "Fiddler", link: "/tools/fiddler/" },
+    { text: "Docker", link: "/tools/docker/" },
+    { text: "Kubernetes", link: "/tools/kubernetes/" },
+    { text: "Jenkins", link: "/tools/jenkins/" },
+    { text: "Nginx", link: "/tools/nginx/" },
+    { text: "Elasticsearch", link: "/tools/elasticsearch/" },
+    { text: "Windows", link: "/tools/windows/" },
+    { text: "Linux", link: "/tools/linux/" },
+  ],
+};
 
-export default toolConfig
+export default toolConfig;
